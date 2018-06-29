26eadc4418ffcae39314090c850b0dae10b2c014
Josh Cheek <josh.cheek@gmail.com>
2018-06-28 23:55:37 -0500

    Refactor lib.rb for legibility

--- lib.rb
+++ lib.rb
@@ -4,11 +4,7 @@ class Sql
   Type    = Struct.new :name, :fields
   Field   = Struct.new :name
   Resolve = Struct.new :type, :resolver
-  Select  = Struct.new :type, :fields, :where do
-    def initialize(type:, fields:, where:)
-      super type, fields, where
-    end
-  end
+  Select  = Struct.new :type, :fields, :where
 
   def define(type:, resolve:)
     t = parse type
@@ -44,39 +40,26 @@ class Sql
     when Hash
       if ast.key? 'CreateStmt'
         create = ast['CreateStmt']
-        Type.new(
-          from_ast(create['relation']),
-          create['tableElts'].map { |h| Field.new from_ast h },
-        )
+        Type.new from_ast(create['relation']),
+                 create['tableElts'].map { |h| Field.new from_ast h }
       elsif ast.key? 'SelectStmt'
         stmt = ast['SelectStmt']
         expr = stmt['whereClause']['A_Expr']
-        Select.new(
-          type:   from_ast(stmt['fromClause'][0]), # => "posts"
-          fields: from_ast(stmt['targetList']),    # => ["id", "title"]
-          where:  [
-            from_ast(expr['name'][0]),      # => "="
-            from_ast(expr['lexpr']).intern, # => :id
-            from_ast(expr['rexpr']),        # => 123
-          ]
-        )
-      elsif ast.key? 'RangeVar'
-        from_ast ast['RangeVar']['relname']
-      elsif ast.key? 'fields'
-        from_ast ast['fields'][0]
-      elsif ast.key? 'colname'
-        from_ast ast['colname']
-      elsif ast.key? 'stmt'
-        from_ast ast['stmt']
-      elsif ast.key? 'val'
-        from_ast ast['val']
-      elsif ast.size == 1
-        from_ast ast.values.first
-      else
-        raise "AST: #{ast.inspect}"
+        Select.new from_ast(stmt['fromClause'][0]),  # => "posts"
+                   from_ast(stmt['targetList']),     # => ["id", "title"]
+                   [ from_ast(expr['name'][0]),      # => "="
+                     from_ast(expr['lexpr']).intern, # => :id
+                     from_ast(expr['rexpr']),        # => 123
+                   ]
+      elsif ast.key? 'RangeVar' then from_ast ast['RangeVar']['relname']
+      elsif ast.key? 'fields'   then from_ast ast['fields'][0]
+      elsif ast.key? 'colname'  then from_ast ast['colname']
+      elsif ast.key? 'stmt'     then from_ast ast['stmt']
+      elsif ast.key? 'val'      then from_ast ast['val']
+      elsif ast.size == 1       then from_ast ast.values.first
+      else raise "AST: #{ast.inspect}"
       end
-    else
-      raise "AST: #{ast.inspect}"
+    else raise "AST: #{ast.inspect}"
     end
   end
 end
